f3be785e0654ad6f9469e8433020a24597632167
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-06-11 21:51:19 -0300

    No longer running pure cvrp inst

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import subprocess
 
 for i in range(1, 37):
 	lp = []
-	for j in range(1, 6):
+	for j in range(2, 6):
 	    lp.append(subprocess.Popen(f'./main.exe instances/2l_cvrp{i:02}{j:02}.txt 3600 results/2l_cvrp{i:02}{j:02}.log results/2l_cvrp{i:02}{j:02}.sol'.split()))
 	for p in lp:
 		p.wait()
