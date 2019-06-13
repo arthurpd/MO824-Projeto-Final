91b0b753e9334bad2a4dce5b479bff3e29c4f90e
Arthur Pratti Dadalto <arthurpd10@gmail.com>
2019-06-12 22:55:42 -0300

    Partial results

--- aggregate_results.py
+++ aggregate_results.py
@@ -76,12 +76,12 @@ Packing
 tsp_time = []
 packing_time = []
 res = []
-n = 20
+n = 10
 for i in range(1, n + 1):
 	tsp_time.append([])
 	packing_time.append([])
 	res.append([])
-	for j in range(1, 6):
+	for j in range(2, 6):
 		f = open(f'results/2l_cvrp{i:02}{j:02}.log', 'r')
 		lines = f.readlines()
 		tsp_time[-1].append(lines[-2][len('Total tsp time '):-1])
@@ -91,7 +91,9 @@ for i in range(1, n + 1):
 	print(i)
 
 print('\n'.join([','.join(line) for line in tsp_time]))
+print()
 print('\n'.join([','.join(line) for line in packing_time]))
+print()
 print('\n'.join([','.join(line) for line in res]))
 
 
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import subprocess
 
-for i in range(1, 37):
+for i in range(21, 37):
 	lp = []
 	for j in range(2, 6):
 	    lp.append(subprocess.Popen(f'./main.exe instances/2l_cvrp{i:02}{j:02}.txt 3600 results/2l_cvrp{i:02}{j:02}.log results/2l_cvrp{i:02}{j:02}.sol'.split()))
